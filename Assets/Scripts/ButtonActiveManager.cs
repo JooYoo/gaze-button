ba1ea9ea1f0eb7b2941f8f7fbd8ea98659824325
JooYoo <jooyoo@outlook.com>
2018-03-02 13:41:18 +0100

    first commit

---
+++ Assets/Scripts/ButtonActiveManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class ButtonActiveManager : MonoBehaviour, IFocusable
+{
+    public GameObject Environment;
+    public GameObject ProgressBar;
+
+    private bool isFocus;
+
+    public void OnFocusEnter()
+    {
+        isFocus = true;
+        StartCoroutine(ProgressBarTimer());
+    }
+
+    public void OnFocusExit()
+    {
+        isFocus = false;
+        // progressBar go back to Zero
+        ProgressBar.transform.localScale =
+            new Vector3(ProgressBar.transform.localScale.x,
+                        0,
+                        ProgressBar.transform.localScale.z);
+
+        StartCoroutine(ProgressBarTimer());
+    }
+
+    IEnumerator ProgressBarTimer()
+    {
+        // progressBar grow up: 如果光标矮于2.4就一直以每0.1秒0.1高的单位进行加载
+        while (ProgressBar.transform.localScale.y <= 2.4f && isFocus == true)
+        {
+            ProgressBar.transform.localScale = ProgressBar.transform.localScale + new Vector3(0, 0.1f, 0);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        if (isFocus)
+        {
+            // progressBar grow up finish then implimente the OnClickBt()，to let the button implement sth
+            Environment.GetComponent<EnvironmentManager>().OnClickBt();
+        }
+    }
+}
---
+++ Assets/Scripts/EnvironmentManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    public GameObject theCube;
+
+    private bool isObjectActive;
+	
+	void Update ()
+	{
+	    isObjectActive = theCube.activeInHierarchy;
+	}
+
+    public void OnClickBt()
+    {
+        theCube.SetActive(!isObjectActive);
+    }
+}
